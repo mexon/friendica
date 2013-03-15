6745ad255627adcc06e6721c68893a3b5ce9efac
Matthew Exon <git.mexon@spamgourmet.com>
2013-03-15 19:51:31 +0800

    Only apply updates if the new edited timestamp is strictly greater than the existing one

--- include/items.php
+++ include/items.php
@@ -1603,26 +1603,23 @@ function dfrn_deliver($owner,$contact,$atom, $dissolve = false) {
 }
 
 
-function item_edited_has_changed($old, &$new) {
-    if (!x($new,'edited')) {
-        return false;
-    }
-    if (!$new['edited']) {
-        return false;
-    }
-    $newedited = datetime_convert('UTC', 'UTC', $new['edited']);
-    $oldedited = datetime_convert('UTC', 'UTC', $old['edited']);
-    $oldretrieved = datetime_convert('UTC', 'UTC', $old['retrieved']);
-
-    $result = false;
-    //if ($newedited != $oldedited) {
-    if (($newedited > $oldretrieved) && ($newedited > $oldedited)) {
-        logger('item has changed (oldretrieved ' . $oldretrieved . ', oldedited ' . $oldedited . ', newedited ' . $newedited . '): ' . $new['plink'], LOGGER_DATA);
-        call_hooks('post_remote', array_merge($old, $new));
-
+/*
+  This function returns true if $update has an edited timestamp newer
+  than $existing.  So that means $update contains new data which
+  should override what's already there.  If either timestamp is empty
+  it assumes that the update is newer.  If the timestamps are equal it
+  returns false.
+  */
+function edited_timestamp_is_newer($existing, $update) {
+    if (!x($existing,'edited') || !$existing['edited']) {
         return true;
     }
-    return false;
+    if (!x($update,'edited') || !$update['edited']) {
+        return true;
+    }
+    $existing_edited = datetime_convert('UTC', 'UTC', $existing['edited']);
+    $update_edited = datetime_convert('UTC', 'UTC', $update['edited']);
+    return (strcmp($existing_edited, $update_edited) < 0);
 }
 
 /**
@@ -2038,7 +2035,7 @@ function consume_feed($xml,$importer,&$contact, &$hub, $datedir = 0, $pass = 0) 
 				// Update content if 'updated' changes
 
 				if(count($r)) {
-					if((x($datarray,'edited') !== false) && (datetime_convert('UTC','UTC',$datarray['edited']) !== $r[0]['edited'])) {  
+					if (edited_timestamp_is_newer($r[0], $datarray)) {  
 
 						// do not accept (ignore) an earlier edit than one we currently have.
 						if(datetime_convert('UTC','UTC',$datarray['edited']) < $r[0]['edited'])
@@ -2190,7 +2187,7 @@ function consume_feed($xml,$importer,&$contact, &$hub, $datedir = 0, $pass = 0) 
 				// Update content if 'updated' changes
 
 				if(count($r)) {
-					if((x($datarray,'edited') !== false) && (datetime_convert('UTC','UTC',$datarray['edited']) !== $r[0]['edited'])) {  
+					if (edited_timestamp_is_newer($r[0], $datarray)) {  
 
 						// do not accept (ignore) an earlier edit than one we currently have.
 						if(datetime_convert('UTC','UTC',$datarray['edited']) < $r[0]['edited'])
@@ -2950,7 +2947,7 @@ function local_delivery($importer,$data) {
 
 				if(count($r)) {
 					$iid = $r[0]['id'];
-					if((x($datarray,'edited') !== false) && (datetime_convert('UTC','UTC',$datarray['edited']) !== $r[0]['edited'])) {
+					if (edited_timestamp_is_newer($r[0], $datarray)) {
 
 						// do not accept (ignore) an earlier edit than one we currently have.
 						if(datetime_convert('UTC','UTC',$datarray['edited']) < $r[0]['edited'])
@@ -3128,7 +3125,7 @@ function local_delivery($importer,$data) {
 				// Update content if 'updated' changes
 
 				if(count($r)) {
-					if((x($datarray,'edited') !== false) && (datetime_convert('UTC','UTC',$datarray['edited']) !== $r[0]['edited'])) {  
+					if (edited_timestamp_is_newer($r[0], $datarray)) {  
 
 						// do not accept (ignore) an earlier edit than one we currently have.
 						if(datetime_convert('UTC','UTC',$datarray['edited']) < $r[0]['edited'])
@@ -3307,7 +3304,7 @@ function local_delivery($importer,$data) {
 			// Update content if 'updated' changes
 
 			if(count($r)) {
-				if((x($datarray,'edited') !== false) && (datetime_convert('UTC','UTC',$datarray['edited']) !== $r[0]['edited'])) {  
+				if (edited_timestamp_is_newer($r[0], $datarray)) {  
 
 					// do not accept (ignore) an earlier edit than one we currently have.
 					if(datetime_convert('UTC','UTC',$datarray['edited']) < $r[0]['edited'])
