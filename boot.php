643490eb1dff56a2bb4d2300708e032f92acecd0
Matthew Exon <git.mexon@spamgourmet.com>
2013-08-10 09:21:37 +0800

    In case of redirects, record the URL that we were redirected to

--- boot.php
+++ boot.php
@@ -408,6 +408,7 @@ if(! class_exists('App')) {
 		private $curl_code;
 		private $curl_content_type;
 		private $curl_headers;
+		private $curl_redirect_url;
 
 		private $cached_profile_image;
 		private $cached_profile_picdate;
@@ -710,6 +711,14 @@ if(! class_exists('App')) {
 
 		function get_curl_headers() {
 			return $this->curl_headers;
+		}
+
+		function set_curl_redirect_url($redirect_url) {
+			$this->curl_redirect_url = $redirect_url;
+		}
+
+		function get_curl_redirect_url() {
+			return $this->curl_redirect_url;
 		}
 
 		function get_cached_avatar_image($avatar_image){
