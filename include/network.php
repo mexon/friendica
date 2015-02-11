6c33ff6c329a950397e13b34f96d5f180ba81c4c
Matthew Exon <mat@exon.name>
2015-02-11 03:55:44 +0000

    set curl code and other info earlier so that the final value is correct after recursion

--- include/network.php
+++ include/network.php
@@ -85,7 +85,10 @@ function fetch_url($url,$binary = false, &$redirects = 0, $timeout = 0, $accept_
 		$base = substr($base,strlen($chunk));
 	}
 
-	$newurl = '';
+	$a->set_curl_code($http_code);
+	$a->set_curl_content_type($curl_info['content_type']);
+	$a->set_curl_headers($header);
+
 	if($http_code == 301 || $http_code == 302 || $http_code == 303 || $http_code == 307) {
 		$new_location_info = @parse_url($curl_info["redirect_url"]);
 		$old_location_info = @parse_url($curl_info["url"]);
@@ -108,11 +111,7 @@ function fetch_url($url,$binary = false, &$redirects = 0, $timeout = 0, $accept_
 		}
 	}
 
-	$a->set_curl_code($http_code);
-	$a->set_curl_content_type($curl_info['content_type']);
-
 	$body = substr($s,strlen($header));
-	$a->set_curl_headers($header);
 
 	$a->save_timestamp($stamp1, "network");
 
